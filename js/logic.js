a488225be3aa4aab5552f712cc1852833c0f66ea
ilyapatis24 <ilyapatis24@mail.ru>
2024-03-05 02:15:46 +0300

    initial commit

---
+++ js/logic.js
@@ -0,0 +1,99 @@
+"use strict";
+
+let gameSymbols = ['x', 'o'];
+let activePlayer = 0;
+let board = [];
+
+let makeField = () => {
+    let scale;
+    do {
+        scale = +prompt(
+            "Введите ширину поля для игры в количестве клеток (не менее 3):"
+        );
+    } while ((scale <= 2 && scale) || !scale);
+    return scale;
+}
+
+function checkFullField(field) {
+    if (board.length > field) {
+        board = [];
+    }
+}
+
+// Создание игрового поля:
+let drawField = (arr, number) => {
+    for (let i = 0; i < number; i++) {
+        arr[i] = [];
+        for (let j = 0; j < number; j++) {
+            arr[i][j] = "";
+        }
+    }
+}
+
+let checkActivePlayer = () => {
+    if (activePlayer === 0) {
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    }
+}
+
+let checkWin = (board, row, column) => {
+    let countRow = 0;
+    let countColumn = 0;
+    let countStepDown = 0;
+    let countStepUp = 0;
+
+    for (let i = 0; i < board.length; i++) {
+        // Проверка строк:
+        if (board[row][i] === gameSymbols[activePlayer]) {
+            countRow += 1;
+        }
+        // Проверка столбцов:
+        if (board[i][column] === gameSymbols[activePlayer]) {
+            countColumn += 1;
+        }
+        // Проверка главной диагонали:
+        if (board[i][i] === gameSymbols[activePlayer]) {
+            countStepDown += 1;
+        }
+        // Проверка побочной диагонали:
+        let j = board[i].length - 1 - i;
+        if (board[j][i] === gameSymbols[activePlayer]) {
+            countStepUp += 1;
+        }
+    }
+
+    if (
+        countRow === board.length ||
+        countColumn === board.length ||
+        countStepDown === board.length ||
+        countStepUp === board.length
+        ) {
+        showWinner(activePlayer);
+    }
+}
+
+let startGame = () => {
+    let field = makeField();
+
+    checkFullField(field);
+
+    drawField(board, field);
+
+    activePlayer = 0;
+
+    renderBoard(board);
+}
+
+let click = (row, column) => {
+    board[row][column] = gameSymbols[activePlayer];
+    checkWin(board, row, column);
+
+    renderBoard(board);
+    console.log(board);
+
+    checkActivePlayer();
+}
+
+
